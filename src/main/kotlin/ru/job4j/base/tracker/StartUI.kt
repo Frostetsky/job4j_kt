0c3fa92ffd0c2dfa60b966831a93223baa45bd7e
19532042 <nik.nesterenko1999@bk.ru>
2023-12-30 22:10:22 +0300

    5. Класс-компаньон. [#191433]

--- src/main/kotlin/ru/job4j/base/tracker/StartUI.kt
+++ src/main/kotlin/ru/job4j/base/tracker/StartUI.kt
@@ -1,24 +1,24 @@
 package ru.job4j.base.tracker
 
-object StartUI {
+class StartUI private constructor() {
 
-    private val tracker = Tracker()
-
-    fun init(numberMenu: Int?) : Boolean {
-        when (numberMenu) {
-            1 -> tracker.add(Item(name = "TestName"))
-            2 -> println(tracker.findAll())
-            else -> return false
+    companion object {
+        fun init(numberMenu: Int?, tracker : Tracker) : Boolean {
+            when (numberMenu) {
+                1 -> tracker.add(Item(name = "TestName"))
+                2 -> println(tracker.findAll())
+                else -> return false
+            }
+            return true
         }
-        return true
     }
 }
 
 fun main(args: Array<String>) {
+    val tracker = Tracker()
     var isNotExit = true;
     while (isNotExit) {
         val numberMenu = readLine()?.toInt()
-        val startUi = StartUI
-        isNotExit = startUi.init(numberMenu)
+        isNotExit = StartUI.init(numberMenu = numberMenu, tracker = tracker)
     }
 }
